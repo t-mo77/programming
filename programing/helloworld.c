56a71bb94452719f1246790913b526ff68ef9eb5
t-mo77 <kom.tomoya@gmail.com>
2025-02-23 19:37:30 +0900

    Delete programing directory

--- programing/helloworld.c
+++
@@ -1,7 +0,0 @@
-//AJG24080 今場智哉
-#include <stdio.h>
-
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
--- programing/kadai1210_1.c
+++
@@ -1,5 +0,0 @@
-//AJG24080 今場智哉
-#include <stdio.h>
-int main(void){
-    printf("Today is December 10.");
-    return 0;}
